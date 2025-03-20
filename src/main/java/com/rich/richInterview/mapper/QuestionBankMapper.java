252e37f15f9ebedeaeb96256d3027db81ba7e3ed
Rich <drc9941@outlook.com>
2025-03-20 20:25:49 +0800

    Rich面试刷题平台后端基础框架搭建完毕。 feat:题库、题目、题目题库关系、用户的基础增删改查业务的实现。

---
+++ src/main/java/com/rich/richInterview/mapper/QuestionBankMapper.java
@@ -0,0 +1,18 @@
+package com.rich.richInterview.mapper;
+
+import com.rich.richInterview.model.entity.QuestionBank;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author duruichi
+* @description 针对表【question_bank(题库)】的数据库操作Mapper
+* @createDate 2025-03-20 17:31:32
+* @Entity com.rich.richInterview.model.entity.QuestionBank
+*/
+public interface QuestionBankMapper extends BaseMapper<QuestionBank> {
+
+}
+
+
+
+
---
+++ src/main/java/com/rich/richInterview/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.rich.richInterview.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rich.richInterview.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
