3da97d7aed5784349fb921c58b94aca55da58fce
Rich <drc9941@outlook.com>
2025-04-15 18:21:34 +0800

    feat:完善关于作者、关于项目页内容。

--- rich-interview-web/src/app/other/aboutAuthor/page.tsx
+++ rich-interview-web/src/app/other/aboutAuthor/page.tsx
@@ -1,12 +1,133 @@
-import styles from "./page.module.css";
+"use client";
+import { Card, Row, Col } from "antd";
+import { ConstantMsg } from "@/constant/ConstantMsg";
+import styles from "../components/page.module.css";
+import { GithubFilled } from "@ant-design/icons";
+import React from "react";
+
+// 数据
+const interviewData = [
+  {
+    direction: "后端",
+    technologyStackRow01:
+      "•熟悉JAVASE相关内容：基础语法、面向对象、常用API、继承和多态、集合、异常、IO流、多线程、锁、网络编程基础、反射",
+    technologyStackRow02: "熟悉springMVC框架下基于JAVAEE的开发生态",
+    technologyStackRow03:
+      "•熟悉JavaWeb项目开发流程，能基于行业规范实现业务逻辑，能自主解决BUG",
+    technologyStackRow04:
+      "•熟悉基于MySQL的数据库内容，主流存储引擎及其结构，了解事务、SQL优化相关内容；掌握Mybites、Mybatis-Plus框架，能熟练地基于业务需求快速实现数据库操作",
+    technologyStackRow05:
+      "•熟悉Redis、ElasticSearch、Kafka等常用中间件，能基于业务需求实现数据的缓存、消息队列、全文检索等功能，如Redis + Caffeine + Hotkey 构建高性能实时缓存",
+  },
+  {
+    direction: "前端",
+    technologyStackRow01: "•熟悉HTML、CSS、JavaScript、TypeScript等前端语言",
+    technologyStackRow02: "•熟悉Vue、React（本项目）框架",
+    technologyStackRow03: "•使用热门组件element、Ant Design快速搭建项目页面",
+  },
+  {
+    direction: "其他",
+    technologyStackRow01:
+      "•了解常见的数据结构与算法,并根据实际应用场景对功能进行优化",
+    technologyStackRow02: "•计算机组成原理",
+    technologyStackRow03: "•计算机操作系统",
+    technologyStackRow04: "•数据库原理",
+    technologyStackRow05: "•计算机网络",
+  },
+];
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <button type="button">这个页面是_关于作者_测试页面</button>
+        {/* 基本信息 */}
+        <Card title="基本信息" className={styles.authorInfo}>
+          <Col span={6} className={styles.avatarContainer}>
+            <div className={styles.avatarFrame}>
+              <img
+                src={ConstantMsg.AUTHOR_AVATAR} // 请替换实际头像路径
+                className={styles.avatarImage}
+                alt="作者头像"
+              />
+            </div>
+          </Col>
+          <Row>
+            <Col span={12} className={styles.itemBorder}>
+              <a
+                href="https://github.com/Moo-Jie"
+                key="github"
+                title="我的GITHUB主页"
+                target="_blank"
+              >
+                我的GITHUB主页
+                <GithubFilled key="GithubFilled" />
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12} className={styles.itemBorder}>
+              姓名：{ConstantMsg.AUTHOR_NAME}
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              职业：Java 全栈开发工程师
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              年龄：21
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              性别：男
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              学校：潍坊科技学院
+            </Col>
+          </Row>
+        </Card>
+
+        {/* 擅长技术栈 */}
+        <Card title="擅长技术栈" className={styles.skillsCard}>
+          <Row gutter={[16, 24]} justify="space-between">
+            {interviewData.map((item, index) => (
+              <Col
+                key={index}
+                xs={24}
+                sm={12}
+                md={12}
+                lg={12}
+                className={styles.skillColumn}
+              >
+                <Card
+                  title={item.direction}
+                  headStyle={{ background: "#f0f5ff" }}
+                  className={styles.boxShadowHover}
+                >
+                  <Row gutter={[8, 8]}>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      {item.technologyStackRow01}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      {item.technologyStackRow02}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      {item.technologyStackRow03}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      {item.technologyStackRow04}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      {item.technologyStackRow05}
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Card>
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
--- rich-interview-web/src/app/other/aboutRichInterview/page.tsx
+++ rich-interview-web/src/app/other/aboutRichInterview/page.tsx
@@ -1,12 +1,130 @@
-import styles from "./page.module.css";
+"use client";
+import { Card, Row, Col } from "antd";
+import { ConstantMsg } from "@/constant/ConstantMsg";
+import styles from "../components/page.module.css";
+import { GithubFilled } from "@ant-design/icons";
+import React from "react";
+
+// 数据
+const interviewData = [
+  {
+    direction: "核心优势",
+    technologyStackRow01:
+      "本系统是个人独立开发，从零开始搭建项目前后端，包括数据库设计、接口设计、功能开发、性能优化等",
+    technologyStackRow02:
+      "本系统使用服务端渲染SSR，更好的搜索引擎优化，更快的首屏加载速度，更好的可访问性",
+    technologyStackRow03:
+      "本系统通过同设备登录互斥监测、实时IP黑白名单、熔断机制、速率管控、分层反爬虫规则强化系统和数据安全防护",
+    technologyStackRow04:
+      "本系统采用连接复用机制、热点实时侦测、多级缓存、高效数据结构等优化性能表现",
+  },
+  {
+    direction: "后端",
+    technologyStackRow01: "SpringBoot + MyBatis-Plus",
+    technologyStackRow02:
+      "Redis 分布式缓存 + Caffeine 本地缓存 + Redission 分布式 + BloomFilter + BitMap",
+    technologyStackRow03: "Elasticsearch 搜索引擎",
+    technologyStackRow04:
+      " HotKey 热点探测 + Sentinel 流量控制 + Nacos 配置中心",
+  },
+  {
+    direction: "前端",
+    technologyStackRow01:
+      "React 18服务端渲染 + Ant Design 组件库 + Ant Design Pro",
+    technologyStackRow02: "Redux 状态管理",
+    technologyStackRow03: "TypeScript + ESLint + Prettier 代码规范",
+    technologyStackRow04: "OpenAPI 风格接口",
+  },
+];
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <button type="button">这个页面是_关于本项目_测试页面</button>
+        {/* 基本信息 */}
+        <Card title="基本信息" className={styles.authorInfo}>
+          <Col span={6} className={styles.avatarContainer}>
+            <div className={styles.avatarFrame}>
+              <img
+                src="/assets/pictures/logo.png"
+                className={styles.avatarImage}
+                alt="项目LOGO"
+              />
+            </div>
+          </Col>
+          <Row>
+            <Col span={12} className={styles.itemBorder}>
+              <a
+                href={ConstantMsg.REPO_URL}
+                key="github"
+                title="项目源码"
+                target="_blank"
+              >
+                项目源码
+                <GithubFilled key="GithubFilled" />
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12} className={styles.itemBorder}>
+              项目名：{ConstantMsg.PROJECT_CHINESE_NAME}
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              类型：刷题/学习/文档/资源/交流
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              维护者：{ConstantMsg.AUTHOR_NAME}
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              当前版本：{ConstantMsg.PROJECT_VERSION}
+            </Col>
+            <Col span={12} className={styles.itemBorder}>
+              更新日期：{ConstantMsg.PROJECT_LAST_UPDATE_TIME}
+            </Col>
+          </Row>
+        </Card>
+
+        {/* 项目技术栈及亮点 */}
+        <Card title="项目技术栈及亮点" className={styles.skillsCard}>
+          <Row gutter={[16, 24]} justify="space-between">
+            {interviewData.map((item, index) => (
+              <Col
+                key={index}
+                xs={24}
+                sm={12}
+                md={12}
+                lg={12}
+                className={styles.skillColumn}
+              >
+                <Card
+                  title={item.direction}
+                  headStyle={{ background: "#f0f5ff" }}
+                  className={styles.boxShadowHover}
+                >
+                  <Row gutter={[8, 8]}>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      •{item.technologyStackRow01}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      •{item.technologyStackRow02}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      •{item.technologyStackRow03}
+                    </Col>
+                    <Col span={12} className={styles.itemBorder}>
+                      {" "}
+                      •{item.technologyStackRow04}
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Card>
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
