2addbc251a1f8e703a0f8cc2e716eeca5b9b202f
Moo-Jie <drc9941@outlook.com>
2025-05-14 21:20:16 +0800

    feat : 前端页面完善。

--- rich-interview-web/config/menu.tsx
+++ rich-interview-web/config/menu.tsx
@@ -18,94 +18,106 @@ import AccessEnumeration from "@/access/accessEnumeration";
  * TODO: 具体菜单待完善
  */
 export const sysMenus = [
-  {
-    path: "/",
-    name: "主页",
-    icon: <HomeOutlined />,
-    access: AccessEnumeration.NOT_LOGIN,
-  },
-  {
-    path: "/banks",
-    name: "题库",
-    icon: <BookOutlined />,
-    access: AccessEnumeration.USER,
-  },
-  {
-    path: "/questions",
-    name: "题目",
-    icon: <QuestionCircleOutlined />,
-    access: AccessEnumeration.USER,
-  },
-  {
-    path: "/admin",
-    name: "管理",
-    icon: <CrownOutlined />,
-    access: AccessEnumeration.ADMIN,
-    children: [
-      {
-        path: "/admin/user",
-        name: "用户管理",
-        icon: <TeamOutlined />,
-        access: AccessEnumeration.ADMIN,
-      },
-      {
-        path: "/admin/bank",
-        name: "题库管理",
-        icon: <DatabaseOutlined />,
-        access: AccessEnumeration.ADMIN,
-      },
-      {
-        path: "/admin/question",
-        name: "题目管理",
-        icon: <EditOutlined />,
-        access: AccessEnumeration.ADMIN,
-      },
-    ],
-  },
-  {
-    name: "其他",
-    path: "/other",
-    icon: <MoreOutlined />,
-    access: AccessEnumeration.NOT_LOGIN,
-    children: [
-      {
-        path: "/other/aboutAuthor",
-        name: "关于作者",
-        icon: <UserOutlined />,
+    {
+        path: "/",
+        name: "主页",
+        icon: <HomeOutlined/>,
         access: AccessEnumeration.NOT_LOGIN,
-      },
-      {
-        path: "/other/aboutRichInterview",
-        name: "关于项目",
-        icon: <InfoCircleOutlined />,
+    },
+    {
+        path: "/banks",
+        name: "题库",
+        icon: <BookOutlined/>,
+        access: AccessEnumeration.USER,
+    },
+    {
+        path: "/questions",
+        name: "题目",
+        icon: <QuestionCircleOutlined/>,
+        access: AccessEnumeration.USER,
+    },
+    {
+        path: "/admin",
+        name: "管理",
+        icon: <CrownOutlined/>,
+        access: AccessEnumeration.ADMIN,
+        children: [
+            {
+                path: "/admin/user",
+                name: "用户管理",
+                icon: <TeamOutlined/>,
+                access: AccessEnumeration.ADMIN,
+            },
+            {
+                path: "/admin/bank",
+                name: "题库管理",
+                icon: <DatabaseOutlined/>,
+                access: AccessEnumeration.ADMIN,
+            },
+            {
+                path: "/admin/question",
+                name: "题目管理",
+                icon: <EditOutlined/>,
+                access: AccessEnumeration.ADMIN,
+            },
+        ],
+    },
+    {
+        name: "其他",
+        path: "/other",
+        icon: <MoreOutlined/>,
         access: AccessEnumeration.NOT_LOGIN,
-      },
-    ],
-  },
+        children: [
+            {
+                path: "/other/aboutAuthor",
+                name: "关于作者",
+                icon: <UserOutlined/>,
+                access: AccessEnumeration.NOT_LOGIN,
+            },
+            {
+                path: "/other/aboutRichInterview",
+                name: "关于项目",
+                icon: <InfoCircleOutlined/>,
+                access: AccessEnumeration.NOT_LOGIN,
+            },
+            {
+                path: "/other/AgreementOrTermPages/userServiceAgreement",
+                name: "用户服务协议",
+                icon: <MoreOutlined/>,
+                access: AccessEnumeration.NOT_LOGIN,
+            },
+            {
+                path: "/other/AgreementOrTermPages/privacyPolicy",
+                name: "隐私政策",
+                icon: <MoreOutlined/>,
+                access: AccessEnumeration.NOT_LOGIN,
+            }
+        ],
+    },
 ] as MenuDataItem[];
 
 // TODO 其他菜单方法待完善
 // 指定路径查找菜单
 export const findMenuByPath = (
-  menus: MenuDataItem[],
-  path: string,
+    menus: MenuDataItem[],
+    path: string,
 ): MenuDataItem | null => {
-  // 遍历菜单列表，递归查找匹配的菜单及其子菜单
-  for (const menu of menus) {
-    if (menu.path === path) {
-      return menu;
+    // 遍历菜单列表，递归查找匹配的菜单及其子菜单
+    for (const menu of menus) {
+        if (menu.path === path) {
+            return menu;
+        }
+        if (menu.children) {
+            const matchedMenuItem = findMenuByPath(menu.children, path);
+            if (matchedMenuItem) {
+                return matchedMenuItem;
+            }
+        }
     }
-    if (menu.children) {
-      const matchedMenuItem = findMenuByPath(menu.children, path);
-      if (matchedMenuItem) {
-        return matchedMenuItem;
-      }
-    }
-  }
-  return null;
+    return null;
 };
 
 // 查找所有菜单
 export const findAllMenuByPath = (path: string): MenuDataItem | null => {
-  return findMenuByPath(sysMenus, path);
+    return findMenuByPath(sysMenus, path);
 };
--- rich-interview-web/src/app/not-found.tsx
+++ rich-interview-web/src/app/not-found.tsx
@@ -12,8 +12,8 @@ export default function NotFound() {
     }}>
         <Result
             status="404"
-            title={<span className={styles.title}> 找不到该页面！ </span>}
-            subTitle="您暂时无法访问此内容，请先登录后再试。"
+            title={<span className={styles.title}> 您暂时无法访问此内容，请先登录后再试。 </span>}
+            subTitle="该页面被拦截，您可能尚未登录！"
             extra={
                 <Button
                     type="primary"
